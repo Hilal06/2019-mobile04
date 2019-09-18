ab32484e2850dfa2c4bb309273c3370a24cb9a70
Hilal06 <rifaulhilal06@gmail.com>
2019-09-18 14:25:45 +0700

    membuat explicit activity, membuat action untuk activity explicit intent

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -6,13 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
+    private EditText input;
+    private TextView viewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+        input = findViewById(R.id.input_name);
+        viewText = findViewById(R.id.text_output);
     }
 
+
+    public void onSubmit(View view) {
+        String txt = input.getText().toString();
+        viewText.setText("Hay " + txt);
+    }
 }
